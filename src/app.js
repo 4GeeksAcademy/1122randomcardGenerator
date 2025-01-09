ff38d9d462dfc4dfb098c825a005870902d69342
1122gs <GORDON.SMITHJR@hotmail.com>
2025-01-09 13:44:59 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const number = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "K", "j"];
+  const suit = ["♦", "♥", "♠", "♣"];
+  let randNumber = Math.floor(Math.random() * 12);
+  let randSuit = Math.floor(Math.random() * 4);
+  let numbs = document.getElementById("numb");
+  numbs.innerHTML = number[randNumber];
+  let newSuit = document.getElementById("topSuit");
+  newSuit.innerHTML = suit[randSuit];
+  if (suit[randSuit] === "♠" || suit[randSuit] == "♣") {
+    newSuit.style.color = "black";
+  } else {
+    newSuit.style.color == "red";
+  }
+  let newSuit2 = document.getElementById("bottomSuit");
+  newSuit2.innerHTML = suit[randSuit];
+
+  if (suit[randSuit] === "♠" || suit[randSuit] === "♣") {
+    newSuit2.style.color = "black";
+  } else {
+    newSuit2.style.color == "red";
+    newSuit2.style.transform = "rotate(180deg)";
+  }
 };
